7ddf00f49819eec1d2a4c4855329397068118f64
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 05:24:19 +0530

    Create TestProgram_1.java

---
+++ TestProgram_1.java
@@ -0,0 +1,9 @@
+public class TestProgram_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Successfully installed");
+		System.out.print("Successfully installed");
+	}
+
+}

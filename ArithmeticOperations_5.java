4c0e6f3e2ca04c039a3ecd6cf13d810fb4191fba
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 06:06:00 +0530

    Create ArithmeticOperations_5.java

---
+++ ArithmeticOperations_5.java
@@ -0,0 +1,42 @@
+
+public class ArithmeticOperations_5 {
+
+	public static void main(String[] args) {
+		//adding variable 
+		int x = 5;
+		int y = 25;
+		
+		//Addition
+		int sum;
+		sum = x+y;
+		System.out.print("The sum of two numbers is:");
+		System.out.println(sum);
+		
+		//Subtraction 
+		int diff;
+		diff = y-x;
+		System.out.print("The difference between two numbers is:");
+		System.out.println(diff);
+		
+		//Multiplication
+		int multi;
+		multi = x*y;
+		System.out.print("The multiplication of two numbers is:");
+		System.out.println(multi);
+		
+		//Division
+		float div;
+		div = y/x;
+		System.out.print("The division of the two numbers is:");
+		System.out.println(div);
+		
+		//Modulo Division
+		float mod;
+		mod = y%x;
+		System.out.print("The module division of the given number is:");
+		System.out.println(mod);
+		
+	}
+	
+
+}

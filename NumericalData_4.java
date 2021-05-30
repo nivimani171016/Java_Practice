1868630cb7b8f7791ff6a5fe42167ce3def98759
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 05:55:06 +0530

    Create NumericalData_4.java

---
+++ NumericalData_4.java
@@ -0,0 +1,33 @@
+public class NumericalData_4 {
+
+	public static void main(String[] args) {
+		//use of int data type 
+		int distance = 28;  // distance is a integer variable
+		System.out.println(distance);
+		
+		
+		// int can also store negative values
+		//storing large value in int shows error as it has its own limit
+		//limits upto 32 bits 
+		
+		//use of long data type 
+		long dis1 = 1489189184116L;
+		System.out.println(dis1);
+		
+		//long and int can't store decimal values
+		
+		//use of float data type 
+		float dis = 25.6f;
+		System.out.println(dis);
+		float dis2 = 25.65481415f; //output will be truncated as float has its own limit
+		System.out.println(dis2);
+		
+		//use of double
+		double dis3 = 25.648116891616;
+		System.out.println(dis3);
+		
+		
+		
+	}
+
+}

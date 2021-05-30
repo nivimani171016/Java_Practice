1f429bec87732e0e2e168a658ddc7304c1a541fb
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 14:04:00 +0530

    Create Exercise_7d_IfElse.java

---
+++ Exercise_7d_IfElse.java
@@ -0,0 +1,32 @@
+public class Exercise_7d_IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 4;
+		int c = 3;
+		if (a>b)
+		{
+			if(a>c)
+			{
+				System.out.println("A is the greater number");
+			}
+			else
+			{
+				System.out.println("C is the greater number");
+			}
+		}
+		else
+		{
+			if (b>c)
+			{
+				System.out.println("B is the greater number");
+			}
+			else
+			{
+				System.out.println("C is the greater number");
+			}
+		}
+	}
+
+}

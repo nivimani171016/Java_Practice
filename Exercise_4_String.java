c8e297ac35190c514ed4c6979ee4787969633f55
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 07:33:46 +0530

    Create Exercise_4_String.java

---
+++ Exercise_4_String.java
@@ -0,0 +1,12 @@
+public class Exercise_4_String {
+
+	public static void main(String[] args) {
+		//use of the concat() function
+		String s1="Hello";
+		String s2="Java";
+		String msg = s1.concat(s2);
+		System.out.println(msg);
+
+	}
+
+}

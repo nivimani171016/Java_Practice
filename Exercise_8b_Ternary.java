4742eb949122b74b447ceb33cf9844609c9a4e62
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 15:06:25 +0530

    Create Exercise_8b_Ternary.java

---
+++ Exercise_8b_Ternary.java
@@ -0,0 +1,13 @@
+
+public class Exercise_8b_Ternary {
+
+	public static void main(String[] args) {
+		// To check the largest among two given number
+		int a = 5;
+		int b = 6;
+		int largest=(a>b)?a:b;
+		System.out.println("The largest among the two numbers is" + " " + largest);
+
+	}
+
+}

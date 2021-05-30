f2c8106dedc71a49558abaefdfdd00a750ad27b4
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 05:44:15 +0530

    Create Exercise_3.java

---
+++ Exercise_3.java
@@ -0,0 +1,9 @@
+public class Exercise_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//fixing system.out.print(hello java
+		System.out.println ("Hello World");
+	}
+
+}

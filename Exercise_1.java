7457acde4e05743aabd745ae63d0b86e11f16bdc
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 05:37:51 +0530

    Create Exercise_1.java

---
+++ Exercise_1.java
@@ -0,0 +1,8 @@
+public class Exercise_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java class name and file name are same in this program");
+	}
+
+}

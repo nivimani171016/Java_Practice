6f7dc5b9d3de1a791507ec3aa929cfa6a36dfe1d
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-03 22:30:19 +0530

    Create DoWhile_16_a.java

---
+++ DoWhile_16_a.java
@@ -0,0 +1,16 @@
+
+public class DoWhile_16_a {
+
+	public static void main(String[] args) {
+		//Print numbers from 50 to 40 in reverse
+		int n = 50;
+		do 
+		{
+			System.out.println(n);
+			n--;
+		}while (n>=40);
+		
+	
+	}
+
+}

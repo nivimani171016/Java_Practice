31b3faef1375821d14fe1cea9c77fd6d4f15f2f8
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-03 22:31:19 +0530

    Create dowhile_16.java

---
+++ dowhile_16.java
@@ -0,0 +1,15 @@
+
+public class dowhile_16 {
+
+	public static void main(String[] args) {
+		// Print 1 to 10 numbers
+		int n=1;
+		do
+		{
+			System.out.println(n);
+			n++;
+		}while(n<=10);
+
+	}
+
+}

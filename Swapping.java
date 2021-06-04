b5ac753bd39c28fb907c742e2a1b7b805d978645
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-04 07:37:19 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,21 @@
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		// To swap two numbers
+		int x=5;
+		int y=0,t=0;
+		if (x>0)
+		{
+			t=x; //Value of x gets stored in the varibale
+			x=y;
+			y=t;
+			
+		}
+		System.out.println("The swapped values are :");
+		System.out.println(t);
+		System.out.println(y);
+		System.out.println(x);
+	}
+
+}

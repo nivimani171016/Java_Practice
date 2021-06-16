c90662acd00a562cb7c6248e88acba15625495f0
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-16 20:17:01 +0530

    Create TestEmployee_21.java

---
+++ TestEmployee_21.java
@@ -0,0 +1,18 @@
+public class TestEmployee_21 {
+
+	public static void main(String[] args) {
+		//Object creation 
+		Employee_21 emp1=new Employee_21();
+		Employee_21 emp2=new Employee_21();
+		emp1.emp_no=445;
+		emp1.emp_name="Titin";
+		emp2.emp_no=856;
+		emp2.emp_name="Sofin";
+		System.out.println ("The employee id is " + emp1.emp_no);
+		System.out.println ("The employee name is " + emp1.emp_name);
+		System.out.println ("The employee id is " + emp2.emp_no);
+		System.out.println ("The employee name is " + emp2.emp_name);
+
+	}
+
+}

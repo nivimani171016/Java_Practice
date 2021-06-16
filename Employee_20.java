080befc484a1190f83b88f868e6a1dc757f29527
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-16 20:15:53 +0530

    Create Employee_20.java

---
+++ Employee_20.java
@@ -0,0 +1,11 @@
+public class Employee_20 
+{
+	int emp_no;
+	String emp_name;
+	void printEmployee()
+	{
+		System.out.println (" The employee number is : " + emp_no);
+		System.out.println (" The employee name is :" + emp_name);
+	}
+
+}

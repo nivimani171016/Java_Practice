a02e8d59544758c2efded71bf707d554df18b17c
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-16 20:19:19 +0530

    Create Student_20.java

---
+++ Student_20.java
@@ -0,0 +1,12 @@
+public class Student_20 {
+	//Properties are roll no and name //variable
+	int roll_no;
+	String name;
+		//Class contains the method too //behaviour
+		void studentDetail() 
+		{	
+			System.out.println("The roll number is "+ roll_no);
+			System.out.println("The name is "+ name);
+		}				
+	
+}

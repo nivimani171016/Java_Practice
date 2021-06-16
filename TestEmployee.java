936228927432f1fd2613d55ef49a55204b9b1ba9
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-16 20:17:33 +0530

    Create TestEmployee.java

---
+++ TestEmployee.java
@@ -0,0 +1,10 @@
+public class TestEmployee {
+	//Creating an object to access the employee class
+
+	public static void main(String[] args) {
+		Employee_20 emp1=new Employee_20();
+		System.out.println ("The Employee class contains " + emp1);
+
+	}
+
+}

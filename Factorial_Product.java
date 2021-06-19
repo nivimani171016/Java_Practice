b68aee8f882908a90e237b660483368d182b405d
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-19 12:00:35 +0530

    Create Factorial_Product.java

---
+++ Factorial_Product.java
@@ -0,0 +1,13 @@
+public class Factorial_Product {
+
+	public static void main(String[] args) {
+		// Factorial of the element 
+		int n = 5,fac=0;
+		for (int i=0;i<=n;i++)
+		{
+			 fac=fac*i;
+		}
+		System.out.println(fac);
+	}
+
+}
